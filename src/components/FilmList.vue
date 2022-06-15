fe1da54590cb5b63e59a357598e05640c10dfb53
vince <vincedinardo21@gmail.com>
2022-06-15 10:44:43 +0200

    Api problem

--- src/components/FilmList.vue
+++ src/components/FilmList.vue
@@ -3,6 +3,8 @@
 </template>
 
 <script>
+import axios from "axios"
+
 export default {
   name: 'FilmList',
   components: {
@@ -11,6 +13,19 @@ export default {
   data(){
     return {
         // API link
+        apiUrl : "https://api.themoviedb.org/3/movie/550?api_key=a52d13991466b7a593490ce84099f0d3",
+        films : []
+    }
+  },
+  create(){
+    this.getFilm();
+  },
+  methods: {
+    getFilm(){
+        axios.get(this.apiUrl).then((result) => {
+            this.films = result.data.response;
+            console.log(result);
+        });
     }
   }
 }
