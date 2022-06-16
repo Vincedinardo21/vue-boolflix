c210e25a8e231a030f8ba706bfee0aaf1af3118c
vince <vincedinardo21@gmail.com>
2022-06-16 15:46:44 +0200

    Hover sulle card

--- src/components/FilmCard.vue
+++ src/components/FilmCard.vue
@@ -3,23 +3,26 @@
         <!-- Immagine copertina -->
         <img :src="'https://image.tmdb.org/t/p/' + 'w342' + filmObject.poster_path" :alt="filmObject.original_title">
 
-        <!-- Titolo -->
-        <div><span><b>Titolo:</b> {{filmObject.title}}</span></div>
+        <div class="text-container">
+            <!-- Titolo -->
+            <div><span><b>Titolo:</b> {{filmObject.title}}</span></div>
         
-        <h4>{{filmObject.name}}</h4>
-        <!-- Titolo originale -->
-        <div><span><b>Titolo originale:</b> {{filmObject.original_title}}</span></div>
+            <!-- <h4>{{filmObject.name}}</h4> -->
+            <!-- Titolo originale -->
+            <div><span><b>Titolo originale:</b> {{filmObject.original_title}}</span></div>
         
-        <!-- Lingua -->
-        <div><span><b>Lingua originale:</b> {{filmObject.original_language}}</span></div>
+            <!-- Lingua -->
+            <div><span><b>Lingua originale:</b> {{filmObject.original_language}}</span></div>
               
-        <!-- <img :src="'../assets/img/flag-' + filmObject.original_language + '.png'" :alt="filmObject.original_title"> -->
+            <!-- <img :src="'../assets/img/flag-' + filmObject.original_language + '.png'" :alt="filmObject.original_title"> -->
        
-        <!-- Voto -->
-        <div class="rate">
-            <span><b>Voto:</b> </span>
-            <font-awesome-icon icon="fa-solid fa-star" v-for="(i) in Math.ceil(filmObject.vote_average / 2)" :key="i"/>
+            <!-- Voto -->
+            <div class="rate">
+                <span><b>Voto:</b> </span>
+                <font-awesome-icon icon="fa-solid fa-star" v-for="(i) in Math.ceil(filmObject.vote_average / 2)" :key="i"/>
+            </div>
         </div>
+        
     </div>
 </template>
 
@@ -38,5 +41,19 @@ export default {
         width: 344px;
         border: 1px solid white;
         margin: 5px;
+        position: relative;
+
+        .text-container {
+            display: none;
+            position: absolute;
+            top: 0;
+        }
+
+        &:hover{
+            .text-container {
+                // border: 2px solid red;
+                display: block;
+            }
+        }       
     }
 </style>
